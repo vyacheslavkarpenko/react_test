eef61adf97837b16f0b63e15cf8ca2b822e98e02
vyacheslavkarpenko <viacheslavkarpenkoju@gmail.com>
2019-09-13 17:35:28 +0300

    commit

--- sutra/app1/src/App.js
+++ sutra/app1/src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+
 
 const App = () => {
     return (
-      <div className="App">
-        simple html 
+      <div>
+        <header>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5hw-2eitnZVya-M55MSudZbCreVdOW1r1ysvTSe2XWjVFA4kXNA'/>
+        </header>
+        <nav>
+          <div>list</div>
+          <div>list</div>
+          <div>list</div>
+          <div>list</div>
+        </nav>
+        <div>
+          Main Content
+        <div>
       </div>
     );
 }
 
 export default App;
+
